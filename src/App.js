6d8b27f8cd1307d5e9e768d4d3ca392ebb7c98a1
unknown <vpupkin@company.com>
2020-11-28 16:17:17 +0200

    Add second excercise

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import Profile from "./components/Profile/profile"
-import user from  './user.json'
+import Profile from './components/Profile/profile'
+import user from './user.json'
+import Statistics from './components/Statistics/statistics'
+import statisticalData from './statistical-data.json'
 
 export default function App() { 
   return (<div>
   <Profile
-  name={user.name}
-  tag={user.tag}
-  location={user.location}
-  avatar={user.avatar}
-  stats={user.stats}
-  />
-  </div>)
+      name={user.name}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}/>
+  <Statistics 
+      title={statisticalData.title}
+      stats={statisticalData}/>
+  </div>
+  )
 }
